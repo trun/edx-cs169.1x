776cd0fd2ade2fb14f23a4d1f7e3d7fa0b8c7bcb
Trevor Rundell <trevor.rundell@gmail.com>
2012-10-04 14:56:30 -0400

    hw1 part4

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,23 +1,33 @@
 class Dessert
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
-    # YOUR CODE HERE
+    @calories < 200
   end
   
   def delicious?
-    # YOUR CODE HERE
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
+
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    super name, calories
+    @flavor = flavor
   end
   
   def delicious?
-    # YOUR CODE HERE
+    if @flavor == 'black licorice'
+      false
+    else
+      super
+    end
   end
 end
